6d5390d35a500baa37ee94a6018d03ccf1e32468
rathi-maya <rathim24it@psnacet.edu.in>
2026-07-15 21:52:43 +0530

    input box and add button

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,7 +1,11 @@
 function Header() {
   return (
-    <div className="">
-        <h1>To-Do List</h1>
+    <div className="min-h-screen flex justify-center items-center bg-[#1d2a63]">
+    <div className="bg-[#1f2f6f]  h-100 w-100  shadow-2xl rounded-2xl">
+        <h1 className="m-5 text-xl text-white">To-Do List</h1>
+        <input type="text" className="font-semibold text-sm text-gray-200 p-1 border border-gray-600 w-70 ml-5 mr-5 rounded-sm" placeholder="Please Enter a New Task" />
+        <button className=" text-sm text-white font-medium bg-amber-300 pl-3 pr-3 border p-1 border-amber-300 rounded-sm cursor-pointer">ADD</button>
+    </div>
     </div>
   )
 }
