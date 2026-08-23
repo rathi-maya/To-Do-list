a17065f349975d81dbc8b73c13daba894ae53bef
rathi-maya <rathim24it@psnacet.edu.in>
2026-08-23 19:03:27 +0530

    post method check

--- Frontend/src/Components/TodoInput.jsx
+++ Frontend/src/Components/TodoInput.jsx
@@ -1,10 +1,28 @@
 import "../styles/TodoInput.css";
+import { useState } from "react";
 
-function TodoInput() {
+function TodoInput({setTodos}) {
+
+  const[title, setTitle] = useState("");
+
+  const handleAdd = async() =>{
+    const response = await fetch("http://localhost:5000/todos",{
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: title
+      })
+    })
+    const data = await response.json();
+    setTodos((prevTodos) => [...prevTodos, data]);
+  }
+
   return (
     <div className="todo-input">
-      <input type="text" placeholder="Enter the task" />
-      <button>ADD</button>
+      <input type="text" value={title} onChange={((e)=> setTitle(e.target.value))} placeholder="Enter the task" />
+      <button onClick={()=>handleAdd()} className="AddBtn">ADD</button>
     </div>
   );
 }
