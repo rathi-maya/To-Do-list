8dd51322ef40716e9f026cdab136c4fc1a21d493
rathi-maya <rathim24it@psnacet.edu.in>
2026-08-23 19:04:28 +0530

    delete handle  method

--- Frontend/src/Components/TodoItem.jsx
+++ Frontend/src/Components/TodoItem.jsx
@@ -1,9 +1,27 @@
+import "../styles/TodoItem.css"
+
 function TodoItem(props) {
+
+  const handleDelete = async ()=>{
+        await fetch(`http://localhost:500/todos/${props.id}`,{
+          method: "DELETE"
+        })
+        console.log("deleted");
+  }
   return (
-    <div>
-       <p>{props.title}</p>
-       <button>Edit</button>
-       <button>Delete</button>
+    <div className="todo-item">
+        <div className="task">
+        <input
+          type="checkbox"
+          checked={props.status === "Completed"}
+          onChange={() => console.log("clicked")}
+        />
+        <span className={props.status === "Completed" ? "Completed" : "notCompleted"}>{props.title}</span>
+      </div>
+       <div>
+       <button className="EditBtn" >Edit</button>
+       <button onClick={()=>handleDelete()} className="DeleteBtn">Delete</button>
+       </div>
     </div>
   )
 }
