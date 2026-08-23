7d258a7207dbd41376a4f946750ef1cc0a1e57a0
rathi-maya <rathim24it@psnacet.edu.in>
2026-08-23 22:37:54 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -78,6 +78,8 @@ app.put("/todos/:id", async(req,res)=>{
     }
 })
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000....");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}....`);
 });
